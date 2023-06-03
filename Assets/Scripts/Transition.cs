b6d3c18bf85bfcb374b7193e04f511fb83438d42
zzpdavid2 <zzpdavid2L@gmail.com>
2023-06-02 23:05:31 -0400

    6.2

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // On collision
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("WEEEE");
+    }
+}
